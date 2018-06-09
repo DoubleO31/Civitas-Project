d8fc46b3a645a450db3b92db7c4e600dfca6befb
Kelly <xiaohandang@gmail.com>
2018-06-09 00:45:49 -0700

    Revert "Revert "Merge pull request #2 from DoubleO31/James's-branch""
    This reverts commit 3eca9874cfde44932654beaabb2c6c59e5693521.

--- Civitas-app/src/Action/AppActions.js
+++ Civitas-app/src/Action/AppActions.js
@@ -2,52 +2,50 @@ import AppDispatcher from '../Dispatcher/AppDispatcher.js';
 
 const AppActions = {
 
+  setCounter: function ( num ){
+    AppDispatcher.dispatch(
+      {
+        actionType: 'setCounter',
+        num: num
+      }
+    );
+  },
 
-  openUploadWindow: function() {
-
-    AppDispatcher.dispatch({
-      actionType: 'openUploadWindow',
-    });
+  getHighlights: function(){
+    AppDispatcher.dispatch(
+      {
+        actionType: 'getHighlights',
+      }
+    );
 
   },
 
-
-  setCounter: function(num) {
-    AppDispatcher.dispatch({
-      actionType: 'setCounter',
-      num: num
-    });
-  },
-
-  getHighlights: function() {
-    AppDispatcher.dispatch({
-      actionType: 'getHighlights',
-    });
-
-  },
-
-  createHighlightHTML: function(idNumber, href, title, desc, src) {
-    AppDispatcher.dispatch({
-      actionType: 'createHighlightHTML',
-      idNumber: idNumber,
-      href: href,
-      title: title,
-      desc: desc,
-      src: src
-    });
+  createHighlightHTML: function( idNumber, href, title, desc, src ){
+    AppDispatcher.dispatch(
+      {
+        actionType: 'createHighlightHTML',
+        idNumber:idNumber,
+        href:href,
+        title:title,
+        desc:desc,
+        src:src
+      }
+    );
 
   },
 
-  createHighlightCSS: function(myStyle, idNumber, bgRedChannel, bgGreenChannel, bgBlueChannel, img) {
-    AppDispatcher.dispatch({
-      actionType: 'createHighlightCSS',
-      myStyle: myStyle,
-      idNumber: idNumber,
-      bgRedChannel: bgRedChannel,
-      bgGreenChannel: bgGreenChannel,
-      bgBlueChannel: bgBlueChannel,
-      img: img
-    });
+  createHighlightCSS: function( myStyle, idNumber, bgRedChannel, bgGreenChannel, bgBlueChannel, img ){
+    AppDispatcher.dispatch(
+      {
+        actionType: 'createHighlightCSS',
+        myStyle:myStyle,
+        idNumber:idNumber,
+        bgRedChannel:bgRedChannel,
+        bgGreenChannel:bgGreenChannel,
+        bgBlueChannel:bgBlueChannel,
+        img:img
+      }
+    );
 
   },
 
