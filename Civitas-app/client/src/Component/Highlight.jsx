34b0c126651e7ca90942556f2e5f333c19b3787d
Alex <k9o8@ugrad.cs.ubc.ca>
2018-07-05 18:15:12 -0700

    working on styling
    fixed: background overlap for each element.
    added: basic styling/position for title and description.
    in progress: trying to test different methods to handle styling. javascript mouse enter/exit, css styling (inline vs external).
    todo: wow! element, figure out where it should go, figure out how to properly identify each image, could run into issues of duplicate filename etc etc.

--- Civitas-app/client/src/Component/Highlight.jsx
+++ Civitas-app/client/src/Component/Highlight.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 let Highlight = function statelessFunctionComponentClass(props) {
-  let source = (props.source);
+  let source = props.source;
   let link = props.link;
   let title = props.title;
   let desc = props.desc;
@@ -9,19 +9,57 @@ let Highlight = function statelessFunctionComponentClass(props) {
     position: 'relative',
     width: '400px',
     height: '400px',
-    background: 'white',
-    display: 'inline-block'
+    background: `url(${source})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'inline-flex',
+    padding: '0px 0px 0px 0px',
+    margin: '5px 5px 5px 5px',
+    /**
+      try to implement the hover functionality to show description.
+      may generate external css sheet, or figure out some how 
+      generate it in-line style. So far does not work. 
+    **/
+    ":hover": {
+      cursor: "pointer",
+      backgroundColor: "rgb(255,0,0)",
+      color: "rgb(255,0,0)"
+    }
   };
 
+  let picStyle = {
+    position: 'center',
+    width: '400px',
+    height: '400px',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+
+  let titleStyle = {
+    position: 'absolute',
+    fontSize:'2em',
+    background: 'grey',
+    textAlign: 'center',
+    top: '5px',
+    width: '400px'
+  }
+
+  let descStyle = {
+    fontSize: '1.1em',
+    position: 'absolute',
+    bottom: '0px',
+    backgroundColor: 'grey',
+
+  }
+
+  let highlightTitleName = source + title + "title";
+  let highlightDescName = source + title + "desc";
 
 
   return (
     <button style = {style} onClick={props.onClick}>
-    <a href={link}>
-    <img src={source} alt={title} style= {style}/>
-    </a>
-    <div id="highlight1-title">{title}</div>
-    <div id="highlight1-desc">{desc}</div>
+    <div id={highlightTitleName} style = {titleStyle}>{title}</div>
+    <div id={highlightDescName} style = {descStyle}>{desc}</div>
     </button>
     );
 };
