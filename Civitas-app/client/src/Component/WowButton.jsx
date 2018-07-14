2633675fa9070f77f80d7e4c6498a2d4013ef940
Alex <k9o8@ugrad.cs.ubc.ca>
2018-07-14 13:24:04 -0700

    wowbutton using react
    test wow button implementation using react, no flux implementation yet.

--- Civitas-app/client/src/Component/WowButton.jsx
+++ Civitas-app/client/src/Component/WowButton.jsx
@@ -1,31 +1,56 @@
-import React from 'react';
-let WowButton = function steatelessFunctionComponentClass(props) {
-	let source = props.source;
-	let arrID = props.arrID;
+import React, { Component } from 'react';
+class WowButton extends React.Component {
+	state = {
+		count: 42,
+		wowed: false
+	};
 
-	let style = {
-		fontSize: '1em',
-		backgroundColor: 'lightblue',
-		color: 'yellow',
-		position: 'absolute',
-		width: 'auto',
-		height: 'auto',
-		textAlign: 'justify',
-		right: '0px',
-		top: '150px',
-		borderRadius: '100%'
+	constructor(props) {
+		super(props);
 	}
 
-	return (
-		<button id={source} style={style} onClick={props.onClick}>
-		Wow! 
-		<div> 29k </div>
+	handleClick = () => {
+		if (!this.state.wowed) {
+		this.setState((prevState, { count, wowed }) =>
+			({
+				count: prevState.count + 1,
+				wowed: !prevState.wowed
+			}));
+	} else {
+		this.setState((prevState, {count, wowed }) => 
+			({
+				count: prevState.count -1,
+				wowed: !prevState.wowed
+			}));
+	}};
+
+	render () {
+		var cssStyle = {
+			fontSize: '.8em',
+			backgroundColor: 'lightblue',
+			color: 'yellow',
+			position: 'relative',
+			width: 'auto',
+			height: 'auto',
+			textAlign: 'justify',
+			right: '0px',
+			top: '0px',
+			borderRadius: '100%',
+			zIndex: '0',
+			display: 'block-Inline'
+		}
+
+		return (
+			<button id={this.props.source} style={cssStyle} onClick={this.handleClick} >
+			Wow! 
+			<div> {this.state.count} </div>
 	{/* need to make a call to server to get number of wows, and when clicked 
 		to $inc wow by 1. Maybe set some timeout so can't keep toggling. Need
-		to also change the colour of the button if pressed?*/}
+	to also change the colour of the button if pressed?*/}
 
-		</button>
-		);
-};
+	</button>
+	);	
+	}
+};	
 
 export default WowButton;
