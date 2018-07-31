b303377b57d1d33b5d33ac60fc2d0cd7dfd3550d
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2018-07-31 14:04:58 -0700

    now useremail save to localstorge.. and added googlemap at dashboard for test

--- client/src/Component/Dashboard/DashboardPage.jsx
+++ client/src/Component/Dashboard/DashboardPage.jsx
@@ -1,59 +1,84 @@
-import React from 'react';
-import Auth from '../../modules/Auth';
-import Menubar from '../Menubar.jsx';
-import Appstore from '../../Stores/AppStore.js'
+import React from "react";
+import Auth from "../../modules/Auth";
+import Menubar from "../Menubar.jsx";
+import Appstore from "../../Stores/AppStore.js";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
+      {props.isMarkerShown && (
+        <Marker position={{lat: -34.397, lng: 150.644}} />
+      )}
+    </GoogleMap>
+  ))
+);
 
 class DashboardPage extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      useremail: '',
-      userinfo: ''
+      useremail: "",
+      userinfo: "",
+      isMarkerShown: false
     };
   }
 
-  callApi = async() =>{
-    const response = await fetch('/usersinfo', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: this.state.useremail
-        })
-      });
+  callApi = async () => {
+    const response = await fetch("/usersinfo", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.state.useremail
+      })
+    });
     const userinfotemp = await response.json();
 
-    if(response.status !== 200) throw Error(userinfotemp.message);
+    if (response.status !== 200) throw Error(userinfotemp.message);
 
     return userinfotemp;
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState(
       {
-      useremail: Appstore.getuserinfo()
-    }, () => {
-      this.callApi()
-      .then(result=>this.setState({userinfo:result[0]}))
-      .catch(err => console.log(err))
-  });
-}
+        useremail: Appstore.getuserinfo()
+      },
+      () => {
+        this.callApi()
+          .then(result => this.setState({userinfo: result[0]}))
+          .catch(err => console.log(err));
+      }
+    );
 
-display(){
-  if (this.state.userinfo!== ''){
-    return this.state.userinfo.name};
+    this.delayedShowMarker();
   }
 
+  display() {
+    if (this.state.userinfo !== "") {
+      return this.state.userinfo.name;
+    }
+  }
 
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({isMarkerShown: true});
+    }, 3000);
+  };
 
-
-
-
+  handleMarkerClick = () => {
+    this.setState({isMarkerShown: false});
+    this.delayedShowMarker();
+  };
 
   /**
    * This method will be executed after initial rendering.
@@ -62,16 +87,23 @@ display(){
    * Render the component.
    */
   render() {
-
     return (
       <div>
-      <Menubar />
-      <h1>Welcome back {this.display()}
-      </h1>
+        <Menubar />
+        <h1>
+          Welcome back {this.display()}
+          <MyMapComponent
+            isMarkerShown={this.state.isMarkerShown}
+            onMarkerClick={this.handleMarkerClick}
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCx55KjQx3iDjCF4RTdo4PD_WfEWLiADVE&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{height: `100%`}} />}
+            containerElement={<div style={{height: `400px`}} />}
+            mapElement={<div style={{height: `100%`}} />}
+          />
+        </h1>
       </div>
     );
   }
-
 }
 
 export default DashboardPage;
--- client/src/Stores/AppStore.js
+++ client/src/Stores/AppStore.js
@@ -1,8 +1,8 @@
-import AppDispatcher from '../Dispatcher/AppDispatcher.js';
-import { EventEmitter } from 'events';
-import Auth from '../modules/Auth';
+import AppDispatcher from "../Dispatcher/AppDispatcher.js";
+import {EventEmitter} from "events";
+import Auth from "../modules/Auth";
 
-const CHANGE_EVENT = 'change';
+const CHANGE_EVENT = "change";
 
 let _uploadWindowOpen = false;
 let _photoViewerOn = false;
@@ -14,54 +14,52 @@ var bgColor = null;
 var _photos = [];
 var _selectedPhoto = {};
 var _wowCount = 0;
-var useremail;
-
 
 function setSelectedPhoto(obj) {
   _selectedPhoto = obj;
 }
 
 function logout() {
-  Auth.deauthenticateUser();}
+  Auth.deauthenticateUser();
+}
 
 function loadWowDetails(data) {
   _wowCount = data[1];
   _wowed = data[0];
 }
 
-
-
-
+function saveuserinfo(data) {
+  localStorage.setItem("userinfo", data);
+}
 
 class Appstore extends EventEmitter {
-
-  emitChange(){
-    this.emit( CHANGE_EVENT);
+  emitChange() {
+    this.emit(CHANGE_EVENT);
   }
 
-  addChangeListener( callback ){
-    this.on( CHANGE_EVENT, callback);
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
   }
 
-  removeChangeListener(callback){
-    this.removeListener (CHANGE_EVENT, callback);
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
   }
 
-  getSelectedPhoto(){
+  getSelectedPhoto() {
     return _selectedPhoto;
   }
 
   getUploadWindowStatus() {
-	return _uploadWindowOpen;
-	}
-
-	getPhotoViewerStatus() {
-  	return _photoViewerOn;
+    return _uploadWindowOpen;
   }
 
-	getSignupWindowStatus() {
-		return _signupWindowOn;
-	}
+  getPhotoViewerStatus() {
+    return _photoViewerOn;
+  }
+
+  getSignupWindowStatus() {
+    return _signupWindowOn;
+  }
 
   getLoginWindowStatus() {
     return _loginWindowOn;
@@ -76,88 +74,82 @@ class Appstore extends EventEmitter {
   }
 
   getuserinfo() {
-    return useremail;
+    return localStorage.getItem("userinfo");
   }
-
 }
 
 const _appStore = new Appstore();
 
 export default _appStore;
 
-_appStore.dispatchToken = AppDispatcher.register( action => {
-  switch ( action.actionType ){
+_appStore.dispatchToken = AppDispatcher.register(action => {
+  switch (action.actionType) {
+    case "setSelectedPhoto":
+      setSelectedPhoto(action.data);
+      _appStore.emitChange();
+      break;
 
+    case "photoViewerOn":
+      _photoViewerOn = true;
+      _appStore.emitChange();
+      break;
 
-    case 'setSelectedPhoto':
-    setSelectedPhoto(action.data);
-    _appStore.emitChange();
-    break;
+    case "photoViewerOff":
+      _photoViewerOn = false;
+      _appStore.emitChange();
+      break;
 
-    case 'photoViewerOn':
-    _photoViewerOn = true;
-    _appStore.emitChange();
-    break;
+    case "signupWindowOn":
+      _signupWindowOn = true;
+      _appStore.emitChange();
+      break;
 
-    case 'photoViewerOff':
-    _photoViewerOn = false;
-    _appStore.emitChange();
-    break;
+    case "signupWindowOff":
+      _signupWindowOn = false;
+      _appStore.emitChange();
+      break;
 
-		case 'signupWindowOn':
-		_signupWindowOn = true;
-		_appStore.emitChange();
-		break;
+    case "loginWindowOn":
+      _loginWindowOn = true;
+      _appStore.emitChange();
+      break;
 
-		case 'signupWindowOff':
-		_signupWindowOn = false;
-		_appStore.emitChange();
-		break;
+    case "loginWindowOff":
+      _loginWindowOn = false;
+      _appStore.emitChange();
+      break;
 
-    case 'loginWindowOn':
-    _loginWindowOn = true;
-    _appStore.emitChange();
-    break;
+    case "toggleUploadWindow":
+      _uploadWindowOpen = !_uploadWindowOpen;
+      _appStore.emitChange();
+      break;
 
-    case 'loginWindowOff':
-    _loginWindowOn = false;
-    _appStore.emitChange();
-    break;
+    case "wowIncrement":
+      loadWowDetails(action.data);
+      _wowed = true;
+      _wowCount = _wowCount + 1;
+      _appStore.emitChange();
+      break;
 
+    case "wowDecrement":
+      loadWowDetails(action.data);
+      _wowed = false;
+      _wowCount = _wowCount - 1;
+      _appStore.emitChange();
+      break;
 
-    case 'toggleUploadWindow':
-  	_uploadWindowOpen = !_uploadWindowOpen;
-    _appStore.emitChange();
-    break;
+    case "logout":
+      logout();
+      _appStore.emitChange();
+      break;
 
-
-    case 'wowIncrement':
-    loadWowDetails(action.data);
-    _wowed = true;
-    _wowCount = _wowCount + 1;
-    _appStore.emitChange();
-    break;
-
-    case 'wowDecrement':
-    loadWowDetails(action.data);
-    _wowed = false;
-    _wowCount = _wowCount - 1;
-    _appStore.emitChange();
-    break;
-
-    case 'logout':
-    logout();
-    _appStore.emitChange();
-    break;
-
-    case 'setUserinfo':
-    useremail = action.data;
-    _appStore.emitChange();
-    break;
-
+    case "setUserinfo":
+      saveuserinfo(action.data);
+      _appStore.emitChange();
+      break;
 
     default:
-    break;
+      break;
   }
 
   return true;
