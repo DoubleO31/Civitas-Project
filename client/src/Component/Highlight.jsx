f126225d8d702fb645842a09f3001f58af973217
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2018-08-04 00:18:57 -0700

    check undefined

--- client/src/Component/Highlight.jsx
+++ client/src/Component/Highlight.jsx
@@ -4,7 +4,6 @@ import GPSbutton from './GPSbutton.jsx';
 import Auth from '../modules/Auth';
 import AppActions from '../Action/AppActions.js';
 
-
 let Highlight = function statelessFunctionComponentClass(props) {
 
   let source = props.source;
@@ -21,33 +20,40 @@ let Highlight = function statelessFunctionComponentClass(props) {
   let highlightStyle = {
     background: `url(${source})`,
     backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundPosition: 'center'
   };
 
   var bgColour = "rgba(" + averageColour[0] + ", " + averageColour[1] + ", " + averageColour[2] + ', 0.8)';
   var luminance = 0.299 * averageColour[0] + 0.587 * averageColour[1] + 0.114 * averageColour[2];
   // console.log("averageColour brightness:");
   // console.log(luminance);
-  var fontColor = (luminance > 160) ? '#020202' : '#FAFAFA';
+  var fontColor = (luminance > 160)
+    ? '#020202'
+    : '#FAFAFA';
   // console.log("fontColor:");
   // console.log(fontColor);
 
   let overlayStyle = {
     background: bgColour,
-    color: fontColor,
+    color: fontColor
   }
 
-
-  return(
-  <span class="highlight" style={highlightStyle} onClick={props.onClick}>
-     <div class="highlight_overlay" style={overlayStyle}>
-       <h3 class="overlay_title">{title}</h3>
-       <p class="overlay_description">{desc}</p>
-       {Auth.isUserAuthenticated() ? <WowButton id = {id} wow = {wow}/>: null}
-       {lat&&long? <GPSbutton lat = {lat} long= {long}/> : null}
-     </div>
-  </span>
-  )
+  return (<span class="highlight" style={highlightStyle} onClick={props.onClick}>
+    <div class="highlight_overlay" style={overlayStyle}>
+      <h3 class="overlay_title">{title}</h3>
+      <p class="overlay_description">{desc}</p>
+      {
+        Auth.isUserAuthenticated()
+          ? <WowButton id={id} wow={wow}/>
+          : null
+      }
+      {
+        lat && long && lat !== 'undefined' && long !== 'undefined'
+          ? <GPSbutton lat={lat} long={long}/>
+          : null
+      }
+    </div>
+  </span>)
 };
 
 export default Highlight;
