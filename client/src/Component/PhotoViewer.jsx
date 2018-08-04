bcf8541f59ea9fe941eb867ed9f8de19b0f1f34e
Kelly <xiaohandang@gmail.com>
2018-08-03 23:08:18 -0700

    Fixed Photoviewer background colour bug

--- client/src/Component/PhotoViewer.jsx
+++ client/src/Component/PhotoViewer.jsx
@@ -2,31 +2,70 @@ import React from 'react';
 import AppActions from '../Action/AppActions.js';
 
 class PhotoViewer extends React.Component {
-  constructor(props){
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  _closePhotoViewer = () => {
-    AppActions.photoViewerOff();
-  }
+	_closePhotoViewer = () => {
+		AppActions.photoViewerOff();
+	}
 
-  render() {
-    let backgroundStyle = {
-      background: this.props.selectedPhoto.averageColour,
-    }
+	render() {
+
+		let bgColour = "#FFFFFF";
+		let fontColour = "#000000";
+
+    console.log("type of selectedPhoto:");
+    console.log(typeof this.props.selectedPhoto);
+    console.log(this.props.selectedPhoto);
+
+		if (this.props.show === true) {
+
+			console.log("selectedPhoto");
+			console.log(this.props.selectedPhoto);
+      console.log("src:");
+      console.log(this.props.selectedPhoto.src);
+      console.log("averageColour:");
+      console.log(this.props.selectedPhoto.averageColour);
+
+			var averageColour = this.props.selectedPhoto.averageColour;
+      console.log(averageColour);
+			bgColour = "rgba(" + averageColour[0] + ", " + averageColour[1] + ", " + averageColour[2] + ', 0.8)';
+			var luminance = 0.299 * averageColour[0] + 0.587 * averageColour[1] + 0.114 * averageColour[2];
+			fontColour = (luminance > 160) ? '#020202' : '#FAFAFA';
+		}
+
+		let backgroundStyle = {
+			background: bgColour,
+			color: fontColour,
+		}
 
 		if (!this.props.show) {
 			return null;
 		}
-		return (
-		<div class='PhotoViewer'>
-      <img src={this.props.selectedPhoto.src} />
-      <div class="textbox" style={backgroundStyle}>
-        <div> {this.props.selectedPhoto.title}</div>
-        <div>{this.props.selectedPhoto.desc}</div>
-      </div>
-      <button onClick={this._closePhotoViewer}>Close Viewer</button>
-    </div>
+		return ( <
+			div class = 'PhotoViewer' >
+			<
+			img src = {
+				this.props.selectedPhoto.src
+			}
+			/> <
+			div class = "textbox"
+			style = {
+				backgroundStyle
+			} >
+			<
+			div > {
+				this.props.selectedPhoto.title
+			} < /div> <
+			div > {
+				this.props.selectedPhoto.desc
+			} < /div> <
+			/div> <
+			button onClick = {
+				this._closePhotoViewer
+			} > Close Viewer < /button> <
+			/div>
 		);
 	}
 
--- server.js
+++ server.js
@@ -18,174 +18,187 @@ const port = process.env.PORT || 5000;
 
 var MongoClient = require("mongodb").MongoClient;
 var uri =
-  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-hizuc.mongodb.net/test?retryWrites=true";
+	"mongodb+srv://m001-student:m001-mongodb-basics@sandbox-hizuc.mongodb.net/test?retryWrites=true";
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.use(bodyParser.json());
 
 MongoClient.connect(
-  uri,
-  {userNewUrlParser: true},
-  (err, db) => {
-    if (err) throw err;
-    var dbo = db.db("civitas");
+	uri, {
+		userNewUrlParser: true
+	}, (err, db) => {
+		if (err) throw err;
+		var dbo = db.db("civitas");
 
-    app.post("/api/highlights", function(req, res) {
-      dbo
-        .collection("highlights")
-        .find({})
-        .toArray((err, result) => {
-          if (err) throw err;
-          res.send(result);
-          console.log("call1");
-        });
-    });
+		app.post("/api/highlights", function(req, res) {
+			dbo
+				.collection("highlights")
+				.find({})
+				.toArray((err, result) => {
+					if (err) throw err;
+					res.send(result);
+					console.log("call1");
+				});
+		});
 
-    var storage = Multer.diskStorage({
-  		destination: (req, file, callback) => {
-  			callback(null, 'temp/image-uploads')
-  		},
-  		filename: (req, file, callback) => {
-  			callback(null, file.fieldname + '-' + Date.now() + file.originalname)
-  		}
-  	});
+		var storage = Multer.diskStorage({
+			destination: (req, file, callback) => {
+				callback(null, 'temp/image-uploads')
+			},
+			filename: (req, file, callback) => {
+				callback(null, file.fieldname + '-' + Date.now() + file.originalname)
+			}
+		});
 
-  var imageUpload = Multer({storage: storage});
+		var imageUpload = Multer({
+			storage: storage
+		});
 
-  	app.post('/mongodbupload', imageUpload.single('image'), (req, res, next) => {
-  		console.log("req.file:");
-  		console.log(req.file.path);
-  		console.log("req.body:");
-  		console.log(req.body);
+		app.post('/mongodbupload', imageUpload.single('image'), (req, res, next) => {
+			console.log("req.file:");
+			console.log(req.file.path);
+			console.log("req.body:");
+			console.log(req.body);
 
-  		var data = req.body;
+			var data = req.body;
 
-  		var image = fs.readFileSync(req.file.path);
-  		var averageColour = colorThief.getColor(image);
-
-
-  		console.log("averageColour:");
-  		console.log(averageColour);
-
-  		data.averageColour = averageColour;
-  		// data.averageColour = bgColour;
-  		// data.averageColour = "";
-
-  		dbo.collection("highlights").insertOne(data, (err, res) => {
-  			if (err) {
-  				throw err;
-  			} else {
-  				console.log("Upload to MongoDB success. Returning 200 status.");
-  			}
-  		});
-
-  		res.sendStatus(200);
-  	});
+			var image = fs.readFileSync(req.file.path);
+			var averageColour = colorThief.getColor(image);
 
 
-  	app.post('/usersinfo', function(request, response, next) {
-  		const data = request.body;
+			console.log("averageColour:");
+			console.log(averageColour);
 
-  		console.log("Server post /userinfo: request.body");
-  		console.log(data);
-  		dbo.collection("users").find({email: data.email}).toArray((err, result) => {
-  			if (err) throw err;
-  			response.send(result);
-  		});
-  	});
-  });
+			data.averageColour = averageColour;
+			// data.averageColour = bgColour;
+			// data.averageColour = "";
 
-    app.post("/usersinfo", function(request, response, next) {
-      const data = request.body;
-      //console.log(data);
-      dbo
-        .collection("users")
-        .find({email: data.email})
-        .toArray((err, result) => {
-          if (err) throw err;
-          response.send(result);
-        });
-    });
+			dbo.collection("highlights").insertOne(data, (err, res) => {
+				if (err) {
+					throw err;
+				} else {
+					console.log("Upload to MongoDB success. Returning 200 status.");
+				}
+			});
 
-    /*
-	db.photos.update(
-    {
-        "_id": ObjectId("54bb201aa3a0f26f885be2a3"),
-        "likes": { "$ne": ObjectId("54bb2244a3a0f26f885be2a4") }
-    },
-    {
-        "$inc": { "likeCount": 1 },
-        "$push": { "likes": ObjectId("54bb2244a3a0f26f885be2a4") }
-    }
-)
+			res.sendStatus(200);
+		});
 
-	*/
 
-    app.post("/mongodbIncWow", function(request, response, next) {
-      var objid = require("mongodb").ObjectID(request.body.id);
-      var userid = request.body.user;
-      // console.log("getting ready to wow" + objid);
-      dbo.collection("highlights").find({wowList: request.body.user});
-      dbo.collection("highlights").updateOne(
-        {
-          _id: objid,
-          wowList: {$ne: userid}
-        },
-        {
-          $inc: {wow: +1},
-          $push: {wowList: userid}
-        }
-      );
+		app.post('/usersinfo', function(request, response, next) {
+			const data = request.body;
 
-      dbo.collection("users").updateOne(
-        {
-          email: userid,
-          wowed: {$ne: objid}
-        },
-        {
-          $push: {wowed: objid}
-        }
-      );
-    });
+			console.log("Server post /userinfo: request.body");
+			console.log(data);
+			dbo.collection("users").find({
+				email: data.email
+			}).toArray((err, result) => {
+				if (err) throw err;
+				response.send(result);
+			});
+		});
 
-    app.post("/mongodbDecWow", function(request, response, next) {
-      var objid = require("mongodb").ObjectID(request.body.id);
-      var userid = request.body.user;
-      // console.log("getting ready to unwow" + objid);
-      dbo.collection("highlights").updateOne(
-        {
-          _id: objid,
-          wowList: userid
-        },
-        {
-          $inc: {wow: -1},
-          $pull: {wowList: userid}
-        }
-      );
 
-      // console.log("unwowed " + objid);
+		app.post("/usersinfo", function(request, response, next) {
+			const data = request.body;
+			//console.log(data);
+			dbo
+				.collection("users")
+				.find({
+					email: data.email
+				})
+				.toArray((err, result) => {
+					if (err) throw err;
+					response.send(result);
+				});
+		});
 
-      dbo.collection("users").updateOne(
-        {
-          email: userid,
-          wowed: objid
-        },
-        {
-          $pull: {wowed: objid}
-        }
-      );
-    });
+		/*
+			db.photos.update(
+		    {
+		        "_id": ObjectId("54bb201aa3a0f26f885be2a3"),
+		        "likes": { "$ne": ObjectId("54bb2244a3a0f26f885be2a4") }
+		    },
+		    {
+		        "$inc": { "likeCount": 1 },
+		        "$push": { "likes": ObjectId("54bb2244a3a0f26f885be2a4") }
+		    }
+		)
 
-    app.get("/mongodbGetWow", function(request, response, next) {
-      var objid = require("mongodb").ObjectID(request.body.id);
-      var userid = request.body.user;
-    });
-  }
-);
+			*/
 
+		app.post("/mongodbIncWow", function(request, response, next) {
+			var objid = require("mongodb").ObjectID(request.body.id);
+			var userid = request.body.user;
+			// console.log("getting ready to wow" + objid);
+			dbo.collection("highlights").find({
+				wowList: request.body.user
+			});
+			dbo.collection("highlights").updateOne({
+				_id: objid,
+				wowList: {
+					$ne: userid
+				}
+			}, {
+				$inc: {
+					wow: +1
+				},
+				$push: {
+					wowList: userid
+				}
+			});
+
+			dbo.collection("users").updateOne({
+				email: userid,
+				wowed: {
+					$ne: objid
+				}
+			}, {
+				$push: {
+					wowed: objid
+				}
+			});
+		});
+
+		app.post("/mongodbDecWow", function(request, response, next) {
+			var objid = require("mongodb").ObjectID(request.body.id);
+			var userid = request.body.user;
+			// console.log("getting ready to unwow" + objid);
+			dbo.collection("highlights").updateOne({
+				_id: objid,
+				wowList: userid
+			}, {
+				$inc: {
+					wow: -1
+				},
+				$pull: {
+					wowList: userid
+				}
+			});
+
+			// console.log("unwowed " + objid);
+
+			dbo.collection("users").updateOne({
+				email: userid,
+				wowed: objid
+			}, {
+				$pull: {
+					wowed: objid
+				}
+			});
+		});
+
+		app.get("/mongodbGetWow", function(request, response, next) {
+			var objid = require("mongodb").ObjectID(request.body.id);
+			var userid = request.body.user;
+		});
+	});
 // connect to the database and load models
 require("./server/models").connect(config.dbUri);
 
@@ -193,7 +206,9 @@ require("./server/models").connect(config.dbUri);
 app.use(express.static("./server/static/"));
 app.use(express.static("./client/dist/"));
 // tell the app to parse HTTP body messages
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 // pass the passport middleware
 app.use(passport.initialize());
 
@@ -231,7 +246,7 @@ app.post('/upload', multer.single('image'), imgUpload.uploadToGcs, function(requ
 
 // Handle React routing, return all requests to React app
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
