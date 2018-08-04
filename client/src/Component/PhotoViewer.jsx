aab356b594b5f6f23563f6de86c0fa44bf9af429
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2018-08-04 01:52:28 -0700

    warnings gone

--- client/src/Component/PhotoViewer.jsx
+++ client/src/Component/PhotoViewer.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import AppActions from "../Action/AppActions.js";
 
 class PhotoViewer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 
 	_closePhotoViewer = () => {
 		AppActions.photoViewerOff();
@@ -15,21 +12,8 @@ class PhotoViewer extends React.Component {
 		let bgColour = "#FFFFFF";
 		let fontColour = "#000000";
 
-    console.log("type of selectedPhoto:");
-    console.log(typeof this.props.selectedPhoto);
-    console.log(this.props.selectedPhoto);
-
 		if (this.props.show === true) {
-
-			console.log("selectedPhoto");
-			console.log(this.props.selectedPhoto);
-      console.log("src:");
-      console.log(this.props.selectedPhoto.src);
-      console.log("averageColour:");
-      console.log(this.props.selectedPhoto.averageColour);
-
 			var averageColour = this.props.selectedPhoto.averageColour;
-      console.log(averageColour);
 			bgColour = "rgba(" + averageColour[0] + ", " + averageColour[1] + ", " + averageColour[2] + ', 0.8)';
 			var luminance = 0.299 * averageColour[0] + 0.587 * averageColour[1] + 0.114 * averageColour[2];
 			fontColour = (luminance > 160) ? '#020202' : '#FAFAFA';
@@ -44,13 +28,14 @@ class PhotoViewer extends React.Component {
 			return null;
 		}
 		return ( <
-			div class = 'PhotoViewer' >
+			div className = 'PhotoViewer' >
 			<
 			img src = {
 				this.props.selectedPhoto.src
 			}
+      alt=""
 			/> <
-			div class = "PhotoViewerTextbox"
+			div className = "PhotoViewerTextbox"
 			style = {
 				backgroundStyle
 			} >
